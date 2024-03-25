58a29d5b63588aa9b396dd4ddaf1fbdb96069ef1
natcardon <carol.cardona@yaydoo.com>
2024-03-25 16:39:18 -0500

    create count of records and create json structure to store cleaned data

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Importing regular expression python module
 import re
+import json
+ # Create an empty list to store JSON objects 
+json_array = []
+# Create a variable to have the max number of records
+records_to_filter = 100
+count = 0
 # Read data
 with open('data-source/access.log', 'r') as file:
     # Read each line of the file
@@ -8,15 +14,26 @@ with open('data-source/access.log', 'r') as file:
         pattern = r'(\S+) - - \[([\w:/]+\s[+\-]\d{4})\] "(GET|POST|PUT|DELETE|PATCH) (.+?)" (\d+)'
         # Finding all possible matches using regular expression
         matches = re.findall(pattern, line)
-        # Printing
         for match in matches:
             # Collecting the results
             ip,timestamp, method, request, status = match
-            # Printing the results
-            print("IP:", ip)
-            print("Timestamp")
-            print("Method:", method)
-            print("Request:", request)
-            print("Status:", status)
-            print("-----------------")
+            # Create a dictionary representing a JSON object
+            json_object = {
+                "ip": ip,
+                "timestamp": timestamp,
+                "method": method,
+                "request": request,
+                "status": status
+            }    
+            # Insert the dictionary into the array    
+            json_array.append(json_object)
+        
+        count = count + 1
+        
+        # This is a break condition because we have a lot of records
+        if count == records_to_filter:
+            break
 
+
+            
+            
