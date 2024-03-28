e19ba8bce39855b2f2d028ed2e5ad308037a409a
natcardon <carol.cardona@yaydoo.com>
2024-03-28 16:05:28 -0500

    Organasing code in funtions and symplify

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Importing regular expression python module
 import re
 import json
 import pandas as pd
@@ -6,115 +5,129 @@ import sys
 import os
 from scipy.stats import zscore
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 6:
-    print("Usage: python main.py <file_route> <records_to_filter> <anomalies_threshold> <bf_threshold>")
-    sys.exit(1)
+file_route = ""
+json_route = ""
+records_to_filter = 0
+anomalies_threshold = 0
+bf_threshold = 0
 
-# Access command-line arguments
-file_route = sys.argv[1]  # First argument
-json_route = sys.argv[2] 
-records_to_filter = int(sys.argv[3])  # second argument
-anomalies_threshold = int(sys.argv[4]) # third argument
-bf_threshold = int(sys.argv[5]) # for argument
 
-# Check if the file exists
-if not os.path.isfile(file_route):
-    print(f"Error: File '{file_route}' does not exist.")
-    sys.exit(1)
+def init_params():
+    global file_route, json_route, records_to_filter, anomalies_threshold, bf_threshold
+   
+    file_route = sys.argv[1]  # First argument
+    json_route = sys.argv[2]  # Second argument
+    records_to_filter = int(sys.argv[3])  # Third argument
+    anomalies_threshold = int(sys.argv[4])  # Fourth argument
+    bf_threshold = int(sys.argv[5])  # Fifth argument
+    
+def check_params():
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 6:
+        print("We're looking for 6 parameters: file_route, records_to_filter, anomalies_threshold ,bf_threshold")
+        sys.exit(1)
 
+def check_input_file():
+    # Check if the file exists
+    if not os.path.isfile(file_route):
+        print(f"Error: File '{file_route}' does not exist.")
+        sys.exit(1)          
+
+def check_int_parse():
 # Check if records_to_filter, anomalies_threshold, and bf_threshold are integers
-try:
-    records_to_filter = int(records_to_filter)
-    anomalies_threshold = int(anomalies_threshold)
-    bf_threshold = int(bf_threshold)
-except ValueError:
-    print("Error: records_to_filter, anomalies_threshold, and bf_threshold must be integers.")
-    sys.exit(1)
-  
-# Create the output directory if it doesn't exist
-output_dir = "output"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    try:
+        int(records_to_filter)
+        int(anomalies_threshold)
+        int(bf_threshold)
+    except ValueError:
+        print("Error: records_to_filter, anomalies_threshold, and bf_threshold must be integers.")
+        sys.exit(1)
+
+def create_output_directory():
+    # Create the output directory if it doesn't exist
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
         
- # Create an empty list to store JSON objects 
-json_array = []
+def read_and_filter_data():
+    count = 0
+    json_array = []
+    # Read data
+    with open(file_route, 'r') as file:
+        # Read each line of the file
+        for line in file:
+            # Regular expression pattern to extract relevant data
+            pattern = r'(\S+) - - \[([\w:/]+\s[+\-]\d{4})\] "(GET|POST|PUT|DELETE|PATCH) (.+?)" (\d+)'
+            # Finding all possible matches using regular expression
+            matches = re.findall(pattern, line)
+            for match in matches:
+                # Collecting the results
+                ip,timestamp, method, request, status = match
+                # Create a dictionary representing a JSON object
+                json_object = {
+                    "ip": ip,
+                    "timestamp": timestamp,
+                    "method": method,
+                    "request": request,
+                    "status": status
+                }    
+                # Insert the dictionary into the array    
+                json_array.append(json_object)
 
-count = 0
+            count = count + 1
+            
+            # This is a break condition because we have a lot of records
+            if count == records_to_filter:
+                break
+    # Creating a Dataframe and returning grouped data by IP address and HTTP method, count requests
+    initial_dataframe = pd.DataFrame(json_array)
+    print("Initial Dataframe after data clean:")
+    print(initial_dataframe)
+    filtered_data = initial_dataframe.groupby(['ip', 'method']).size().reset_index(name='request_count')  
+    print("Requests by ip count results:")
+    print(filtered_data)
+    return filtered_data     
 
-# Read data
-with open(file_route, 'r') as file:
-    # Read each line of the file
-    for line in file:
-        # Regular expression pattern to extract relevant data
-        pattern = r'(\S+) - - \[([\w:/]+\s[+\-]\d{4})\] "(GET|POST|PUT|DELETE|PATCH) (.+?)" (\d+)'
-        # Finding all possible matches using regular expression
-        matches = re.findall(pattern, line)
-        for match in matches:
-            # Collecting the results
-            ip,timestamp, method, request, status = match
-            # Create a dictionary representing a JSON object
-            json_object = {
-                "ip": ip,
-                "timestamp": timestamp,
-                "method": method,
-                "request": request,
-                "status": status
-            }    
-            # Insert the dictionary into the array    
-            json_array.append(json_object)
+def build_output_json(df):
+    # Convert the DataFrame to JSON format
+    json_result = df.to_json(orient='records')
+    # Write the JSON to a file
+    with open(json_route, 'w') as file:
+        file.write(json_result)
         
-        count = count + 1
-        
-        # This is a break condition because we have a lot of records
-        if count == records_to_filter:
-            break
-        
-        
-# Creating a Dataframe with the filtered data 
-df = pd.DataFrame(json_array)
+def calculate_anomalies(df):
+    # Calculate the z-score for request_count
+    df['request_count_zscore'] = zscore(df['request_count'])
+    # Define a threshold for anomalies (e.g., z-score greater than 3)
+    threshold = anomalies_threshold
+    # Identify anomalies
+    anomalies = df[df['request_count_zscore'] > threshold]
+    # Calculate the proportion of anomalies
+    proportion_anomalies = len(anomalies) / len(df)* 100
+    print("Proportion of anomalies found:", proportion_anomalies)
+    
+def calculate_brute_force_attacks(df):    
+    # Group DataFrame by IP address and sum the request counts
+    ip_request_counts = df.groupby('ip')['request_count'].sum()
+    # Identify potential brute force attacks
+    potential_brute_force = ip_request_counts[ip_request_counts > bf_threshold]
+    # Print IP addresses involved in potential brute force attacks
+    if not potential_brute_force.empty:
+        print("Potential Brute Force Attacks Results:")
+        print(potential_brute_force)
+    else:
+        print("No potential brute force attacks detected.")
 
-# Group data by IP address and HTTP method, count requests
-grouped = df.groupby(['ip', 'method']).size().reset_index(name='request_count')
+check_params()
+init_params()
+check_input_file()
+check_int_parse()
+create_output_directory()              
+df = read_and_filter_data()
+build_output_json(df)
+calculate_anomalies(df)
+calculate_brute_force_attacks(df)
 
-# Convert the DataFrame to JSON format
-json_result = grouped.to_json(orient='records')
 
-# Write the JSON to a file
-with open(json_route, 'w') as file:
-    file.write(json_result)
-
-
-df = pd.DataFrame(grouped)
-
-# Calculate the z-score for request_count
-df['request_count_zscore'] = zscore(df['request_count'])
-
-# Define a threshold for anomalies (e.g., z-score greater than 3)
-threshold = anomalies_threshold
-
-# Identify anomalies
-anomalies = df[df['request_count_zscore'] > threshold]
-
-# Calculate the proportion of anomalies
-proportion_anomalies = len(anomalies) / len(df)* 100
-
-print("Proportion of anomalies found:", proportion_anomalies)
-
-# Define a threshold for the number of requests to consider as a brute force attack
-brute_force_threshold = bf_threshold
-
-# Group DataFrame by IP address and sum the request counts
-ip_request_counts = df.groupby('ip')['request_count'].sum()
-
-# Identify potential brute force attacks
-potential_brute_force = ip_request_counts[ip_request_counts > brute_force_threshold]
-
-# Print IP addresses involved in potential brute force attacks
-if not potential_brute_force.empty:
-    print("Potential Brute Force Attacks:")
-    print(potential_brute_force)
-else:
-    print("No potential brute force attacks detected.")
 
             
